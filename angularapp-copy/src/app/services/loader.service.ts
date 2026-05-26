285299ae6016dff2e63356cdc9aae4c45feadf9b
MOHAMMED AMEER KHAN <ameerkhan@MOHAMMEDs-MacBook-Air.local>
2026-05-26 15:32:27 +0530

    Fix loader styling and add debounce

--- angularapp-copy/src/app/services/loader.service.ts
+++ angularapp-copy/src/app/services/loader.service.ts
@@ -8,19 +8,25 @@ export class LoaderService {
   public isLoading = new BehaviorSubject<boolean>(false);
   public context = new BehaviorSubject<string>('DEFAULT');
   private requestCount = 0;
+  private timeoutId: any;
 
   constructor() {}
 
   show(ctx: string = 'DEFAULT') {
     this.requestCount++;
     this.context.next(ctx);
-    this.isLoading.next(true);
+    if (this.requestCount === 1) {
+      this.timeoutId = setTimeout(() => {
+        this.isLoading.next(true);
+      }, 300); // 300ms delay to prevent redundant flashing
+    }
   }
 
   hide() {
     this.requestCount--;
     if (this.requestCount <= 0) {
       this.requestCount = 0;
+      clearTimeout(this.timeoutId);
       this.isLoading.next(false);
       this.context.next('DEFAULT');
     }
